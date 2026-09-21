a2370a002c8efcca80803c24c1d80487c14f0e9f
Daniil Lazarchuk <lazarhuk410@gmail.com>
2026-09-21 20:58:49 +0300

    add measurement list view and templates

--- djangosite/djangoapp/views.py
+++ djangosite/djangoapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import os
+from django.views import generic
+from django.conf import settings
+from djangoapp.models import CO2Measurement
 
-# Create your views here.
+class MeasurementList(generic.ListView):
+    model = CO2Measurement
+    context_object_name = 'measurements'
+    template_name = 'measurement_list.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            file_path = os.path.join(settings.BASE_DIR, 'test_data')
+            with open(file_path, 'r') as measurement_file:
+                for value in measurement_file:
+                    co2_val = float(value.strip())
+                    CO2Measurement.objects.get_or_create(
+                        sensor_name="Test-Sensor",
+                        co2_level=co2_val
+                    )
+        except (IOError, ValueError):
+            pass
+        return super(MeasurementList, self).get(request, *args, **kwargs)
