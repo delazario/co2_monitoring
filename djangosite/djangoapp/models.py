897d9f18af9fc0cc6ad7d82ca1a51641fc482654
Daniil Lazarchuk <lazarhuk410@gmail.com>
2026-09-21 20:25:22 +0300

    add co2 model and migration

--- djangosite/djangoapp/models.py
+++ djangosite/djangoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CO2Measurement(models.Model):
+    sensor_name = models.CharField(max_length=100)
+    co2_level = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sensor_name} - {self.co2_level} ppm"
