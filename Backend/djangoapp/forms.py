8c5b30be6ab33c121c1c8a0d143d079bce670457
Daniil Lazarchuk <lazarhuk410@gmail.com>
2026-09-23 20:56:22 +0300

    integrate bootstrap 5 ui and create base template

--- Backend/djangoapp/forms.py
+++ Backend/djangoapp/forms.py
@@ -5,3 +5,7 @@ class MeasurementForm(forms.ModelForm):
     class Meta:
         model = CO2Measurement
         fields = '__all__'
+        widgets = {
+            'co2_level': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'For example: 450 ppm'}),
+            'sensor_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Sensor Name...'}),
+        }
